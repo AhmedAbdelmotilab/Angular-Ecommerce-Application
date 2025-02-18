370dc77764e755a84941114c7ce2774f8a2479b7
AhmedAbdelmotilab <AhmedAbdelhakimAbdelmotilab@gmail.com>
2025-02-19 01:30:03 +0200

    Modify All Order Styling

--- src/app/pages/allorders/allorders.component.ts
+++ src/app/pages/allorders/allorders.component.ts
@@ -1,48 +1,60 @@
-import { Component , inject , OnDestroy , OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { jwtDecode } from 'jwt-decode';
 import { IUser } from '../../shared/interfaces/iuser';
 import { OrdersService } from '../../core/services/orders/orders.service';
 import { Subscription } from 'rxjs';
 import { IOrder } from '../../shared/interfaces/iorder';
-import { CurrencyPipe } from '@angular/common';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
-@Component ( {
-    selector : 'app-allorders' ,
-    imports : [ CurrencyPipe ] ,
-    templateUrl : './allorders.component.html' ,
-    styleUrl : './allorders.component.scss'
-} )
-export class AllordersComponent implements OnInit , OnDestroy {
-    userData : IUser | null = {} as IUser;
-    userId : string = '';
-    orders : IOrder[] = [];
-    subscribeGetUserOrders : Subscription = new Subscription ();
+@Component({
+    selector: 'app-allorders',
+    imports: [CurrencyPipe, DatePipe],
+    templateUrl: './allorders.component.html',
+    styleUrl: './allorders.component.scss',
+})
+export class AllordersComponent implements OnInit, OnDestroy {
+    userData: IUser | null = {} as IUser;
+    userId: string = '';
+    orders: IOrder[] = [];
+    subscribeGetUserOrders: Subscription = new Subscription();
 
-    private readonly ordersService = inject ( OrdersService );
+    private readonly ordersService = inject(OrdersService);
 
-    getUserId () : void {
-        this.userData = jwtDecode ( localStorage.getItem ( 'userToken' )! );
-        console.log ( this.userData );
-        this.userId = this.userData?.id !;
-        console.log ( this.userId );
+    getUserId(): void {
+        this.userData = jwtDecode(localStorage.getItem('userToken')!);
+        console.log(this.userData);
+        this.userId = this.userData?.id!;
+        console.log(this.userId);
     }
 
-    getUserOrders ( id : string ) : void {
-        this.subscribeGetUserOrders = this.ordersService.getUserOrders ( id ).subscribe ( {
-            next : ( res ) => {
-                console.log ( res );
-                this.orders = res;
-            }
-        } );
+    getUserOrders(id: string): void {
+        this.subscribeGetUserOrders = this.ordersService
+            .getUserOrders(id)
+            .subscribe({
+                next: (res) => {
+                    console.log(res);
+                    this.orders = res;
+                },
+            });
     }
 
-    ngOnInit () {
-        this.getUserId ();
-        this.getUserOrders ( this.userId );
+    sortedOrders(): IOrder[] {
+        return this.orders
+            .slice()
+            .sort(
+                (a, b) =>
+                    new Date(b.createdAt).getTime() -
+                    new Date(a.createdAt).getTime()
+            );
     }
 
-    ngOnDestroy () {
-        this.subscribeGetUserOrders.unsubscribe ();
-        console.log ( 'UnsubscribeGetUserOrdersDone' );
+    ngOnInit() {
+        this.getUserId();
+        this.getUserOrders(this.userId);
+    }
+
+    ngOnDestroy() {
+        this.subscribeGetUserOrders.unsubscribe();
+        console.log('UnsubscribeGetUserOrdersDone');
     }
 }
