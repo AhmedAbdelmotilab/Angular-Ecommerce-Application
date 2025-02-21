00ccb958ace79adaacf9cac3931e10bd8c7e917d
AhmedAbdelmotilab <AhmedAbdelhakimAbdelmotilab@gmail.com>
2025-02-21 18:06:30 +0200

    Brands + Update UI

--- src/app/pages/brands/brands.component.ts
+++ src/app/pages/brands/brands.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component , inject , OnDestroy , OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { BrandsService } from '../../core/services/brands/brands.service';
+import { IBrand } from '../../shared/interfaces/ibrand';
+import { DatePipe , TitleCasePipe , UpperCasePipe } from '@angular/common';
 
-@Component({
-  selector: 'app-brands',
-  imports: [],
-  templateUrl: './brands.component.html',
-  styleUrl: './brands.component.scss'
-})
-export class BrandsComponent {
+@Component ( {
+    selector : 'app-brands' ,
+    imports : [
+        DatePipe ,
+        UpperCasePipe ,
+        TitleCasePipe
+    ] ,
+    templateUrl : './brands.component.html' ,
+    styleUrl : './brands.component.scss'
+} )
+export class BrandsComponent implements OnInit , OnDestroy {
+    brands : IBrand[] = [];
+    subscribeGetAllBrands : Subscription = new Subscription ();
+    private readonly brandsService = inject ( BrandsService );
 
+    getAllBrands () : void {
+        this.subscribeGetAllBrands = this.brandsService.getAllBrands ().subscribe ( {
+            next : ( res ) => {
+                console.log ( res );
+                this.brands = res.data;
+                console.log ( this.brands );
+            }
+        } );
+    }
+
+    ngOnInit () {
+        this.getAllBrands ();
+    }
+
+    ngOnDestroy () {
+        this.subscribeGetAllBrands.unsubscribe ();
+        console.log ( 'UnsubscribeGetAllBrandsDone' );
+    }
 }
